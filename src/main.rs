8fffc2fd4f980e944ab90cf13ab1d51dfba59ea1
2323-code <nekodoshi23@gmail.com>
2024-11-20 00:28:40 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, world!" }));
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
